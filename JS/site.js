bd74e64d989493f12224bfcc1fe919ee526b68a2
Andy Kukuc <jeep98.ak@gmail.com>
2018-10-03 08:33:05 -0500

    created the javascript folder

---
+++ JS/site.js
@@ -0,0 +1,11 @@
+/*Andy Kukuc
+Professor Krieglstad
+Lab 3 Javascript
+10/3/2018*/
+
+//jQuery 3.x-style ready event and locaaly scoped $
+jQuery(function($) {
+	$('html').removeClass('nojs');
+	$('html').addClass('hasjs');
+});
+
